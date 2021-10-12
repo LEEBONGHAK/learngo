9c41f6731540e3c2412745ac00bed038e1b1a797
octo <dlqhdgkr456@gmail.com>
2021-10-13 01:06:26 +0900

    1.1 Packages and Imports

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+// module을 export하고 싶으면 함수 이름의 첫글자를 대문자로 해야함
+import (
+	"fmt"
+
+	"github.com/LEEBONGHAK/learngo/something"
+)
 
 func main() {
+	// fmt : formatting을 관리하는 package
 	fmt.Println("Hello World")
+	something.SayHello()
 }
