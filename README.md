5b61bd67ae7f2a86b633cd3ad32e4cddde06c09e
octo <dlqhdgkr456@gmail.com>
2021-10-14 23:51:24 +0900

    1.11 Structs

--- README.md
+++ README.md
@@ -313,4 +313,21 @@ func main() {
   
 ## Structs  
   
-	
+struct : object와 비슷하면서 map보다 더 유연함 / structure 같은 것  
+```
+package main
+
+import "fmt"
+
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
+func main() {
+	favFood := []string{"kimchi", "ramen"}
+	person1 := person{name: "bonghak", age: 26, favFood: favFood}
+	fmt.Println(person1)
+}
+```  
--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	// map : python이나 JS의 object 같은 것
-	test := map[string]string{"name": "bonghak", "age": "12"}
-	fmt.Println(test)
-	for key, value := range test {
-		fmt.Println(key, value)
-	}
+	// struct : object와 비슷하면서 map보다 더 유연함 / structure 같은 것
+	favFood := []string{"kimchi", "ramen"}
+	person1 := person{name: "bonghak", age: 26, favFood: favFood}
+	fmt.Println(person1)
 }
