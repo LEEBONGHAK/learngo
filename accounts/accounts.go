f7565e3be5769479488e0bd61e78ceed2920b779
octo <dlqhdgkr456@gmail.com>
2021-10-15 11:37:21 +0900

    2.2 Method part Two with Error

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,11 +1,16 @@
 package accounts
 
+import "errors"
+
 // account struct
 type account struct {
 	// 소문자일 경우 private, 첫글자가 대문자면 public
 	owner   string
 	balance int
 }
+
+// error's name should be started 'err'
+var errNoMoney = errors.New("Can't withdraw")
 
 // struct의 수정을 막기 위해서 struct를 만드는 함수
 func NewAccount(owner string) *account {
@@ -14,8 +19,18 @@ func NewAccount(owner string) *account {
 }
 
 // Deposit method
-func (accountName account) Deposit(amount int) {
+func (accountName *account) Deposit(amount int) {
 	accountName.balance += amount
+}
+
+// Withdraw method
+func (accountName *account) Withdraw(amount int) error {
+	if accountName.balance < amount {
+		return errNoMoney
+	}
+	accountName.balance -= amount
+	// nill : like none or null
+	return nil
 }
 
 // Balance of account
--- main.go
+++ main.go
@@ -10,4 +10,11 @@ func main() {
 	account := accounts.NewAccount("bonghak")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		// Fatall()n : 프로그램 종료 함수
+		//log.Fatalln(err)
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
